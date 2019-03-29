aaf8e945a466e1ae7801cef0a77769c9ed4639ed
Khalid Alam <kalam034@uottawa.ca>
2019-03-29 17:12:46 -0400

    DEV-Issue#8-kalam034

--- MeasureResponseTime.py
+++ MeasureResponseTime.py
@@ -1,6 +1,8 @@
 import csv
 import datetime
 
+import matplotlib.pyplot as plt
+import pandas as pd
 import requests
 
 
@@ -29,4 +31,30 @@ def measure_response_time(url, criteria):
     return out_path
 
 
-path_to_csv = measure_response_time('https://jsonmock.hackerrank.com/api/countries/', "Criteria C")
+def generate_graph(path):
+    '''
+    Saves a histogram with average response time per number of requests
+
+    :param path: Path to a csv file
+    '''
+    response_times = pd.read_csv(path)
+
+    criteria_dict = response_times.groupby("criteria")["responseTime"].apply(list).to_dict()
+    critera_keys = list(criteria_dict.keys())
+    criteria_values = list(criteria_dict.values())
+
+    plt.style.use("seaborn-deep")
+    plt.hist(x=criteria_values, bins=30, label=critera_keys)
+    plt.legend(loc="upper right")
+    plt.xlabel("Response Time in Seconds")
+    plt.ylabel("Number of Requests")
+    plt.savefig("responseTimes-figure")
+    plt.show()
+
+
+for i in range(100):
+    path_to_csv = measure_response_time('https://jsonmock.hackerrank.com/api/countries/', "Criteria A")
+    path_to_csv = measure_response_time('https://jsonmock.hackerrank.com/api/countries/', "Criteria B")
+    path_to_csv = measure_response_time('https://jsonmock.hackerrank.com/api/countries/', "Criteria C")
+
+generate_graph("Response-Times.csv")
